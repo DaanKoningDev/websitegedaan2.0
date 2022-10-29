2b474c2050ba3bafdc46d49138c010e017101755
Daan <112382384+DaanKoningDev@users.noreply.github.com>
2022-10-29 12:34:29 +0200

    Add files via upload

---
+++ extra/extra.js
@@ -0,0 +1,10 @@
+// NAVBAR
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
